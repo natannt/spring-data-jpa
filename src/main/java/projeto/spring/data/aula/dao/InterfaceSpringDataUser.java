3dba4056fcf910d555d8b4d22b8ade5183330620
Natan Dadalt <natan.db@gmail.com>
2023-03-23 21:39:00 -0300

    Consulta customizadas com @Query

--- src/main/java/projeto/spring/data/aula/dao/InterfaceSpringDataUser.java
+++ src/main/java/projeto/spring/data/aula/dao/InterfaceSpringDataUser.java
@@ -1,5 +1,8 @@
 package projeto.spring.data.aula.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +11,7 @@ import projeto.spring.data.aula.model.UsuarioSpringData;
 @Repository
 public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long> {
 	
+	@Query(value = "select p from UsuarioSpringData p where p.nome like %?1%")
+	public List<UsuarioSpringData> buscaPorNome (String nome);
 
 }
--- src/test/java/projeto/spring/data/aula/AppSpringDataTeste.java
+++ src/test/java/projeto/spring/data/aula/AppSpringDataTeste.java
@@ -1,5 +1,6 @@
 package projeto.spring.data.aula;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
@@ -84,7 +85,23 @@ public class AppSpringDataTeste {
 		
 		interfaceSpringDataUser.delete(usuarioSpringData.get());
 
+	}
+	
+	@Test
+	public void testeConsultaNome() {
 		
+		List<UsuarioSpringData> list = interfaceSpringDataUser.buscaPorNome("Natan");
+		
+		for (UsuarioSpringData usuarioSpringData : list) {
+			System.out.println(usuarioSpringData.getEmail());
+			System.out.println(usuarioSpringData.getNome());
+			System.out.println(usuarioSpringData.getIdade());
+			System.out.println(usuarioSpringData.getLogin());
+			System.out.println(usuarioSpringData.getSenha());
+			System.out.println(usuarioSpringData.getId());
+			System.out.println("----------------------------------------------------");
+			
+		}
 	}
 
 }
